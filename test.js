50a12325793318b5184b82b8b4dab973236df8f0
Robin Millette <robin@millette.info>
2018-12-11 12:55:47 -0500

    Revert unit tests.

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 // npm
 import test from "ava"
 
+// core
+import { AssertionError } from "assert"
+
 // self
 import DocsDb from "."
 
-test("first test, woot", (t) => {
+test("create and update doc", (t) => {
   const db = new DocsDb()
   const { _rev } = db.updateDoc({ _id: "joe", fee: 1 })
   const s1 = db.size
@@ -18,4 +21,19 @@ test("first test, woot", (t) => {
   const s3 = db.size
   t.is(db.docCount, 1)
   t.not(s1, s3)
+})
+
+test("revert doc (nothing)", (t) => {
+  const db = new DocsDb()
+  t.throws(() => db.revertDoc("joe"), AssertionError, "nothing to revert")
+})
+
+test("revert doc", (t) => {
+  const db = new DocsDb()
+  const { _rev } = db.updateDoc({ _id: "joe", fee: 1 })
+  const doc = db.updateDoc({ _id: "joe", fee: 2 }, _rev)
+  t.is(doc.fee, 2)
+  t.throws(() => db.revertDoc("joe"), AssertionError, "revs should match")
+  const doc2 = db.revertDoc("joe", doc._rev)
+  t.is(doc2.fee, 1)
 })
