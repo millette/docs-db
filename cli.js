c5ee14b6c20e26faf5229154de31ca779634a76f
Robin Millette <robin@millette.info>
2018-12-11 12:07:56 -0500

    Custom prettier config.

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
-"use strict";
+"use strict"
 
 // self
-const DocsDb = require(".");
+const DocsDb = require(".")
 // const it = require('./doop.json')
 
-const db = new DocsDb();
+const db = new DocsDb()
 
 /*
 db.updateDoc({ _id: 'joe', fee: 0 })
@@ -27,20 +27,20 @@ setTimeout(() => {
 }, 500)
 */
 
-console.log("s0");
-const { _rev } = db.updateDoc({ _id: "joe", fee: 1 });
-console.log("s1", _rev);
+console.log("s0")
+const { _rev } = db.updateDoc({ _id: "joe", fee: 1 })
+console.log("s1", _rev)
 // db.updateDoc({ _id: 'joe2', fee: 22 })
 // console.log('s2')
-const d2 = db.updateDoc({ _id: "joe", fee: 2 }, _rev);
-const d2Rev = d2._rev;
-console.log("d2Rev", d2Rev);
-const d3 = db.deleteDoc("joe", d2Rev);
-const d3Rev = d3._rev;
-db.deleteDoc("joe", d3Rev);
-const d4 = db.deleteDoc("joe", d3Rev);
-const d4Rev = d4._rev;
-db.deleteDoc("joe", d4Rev);
+const d2 = db.updateDoc({ _id: "joe", fee: 2 }, _rev)
+const d2Rev = d2._rev
+console.log("d2Rev", d2Rev)
+const d3 = db.deleteDoc("joe", d2Rev)
+const d3Rev = d3._rev
+db.deleteDoc("joe", d3Rev)
+const d4 = db.deleteDoc("joe", d3Rev)
+const d4Rev = d4._rev
+db.deleteDoc("joe", d4Rev)
 
 // console.log(db._db)
 // console.log(db.sizeCurrent, db.sizeHistory, db.size, db.docCount, db.historyCount)
--- index.js
+++ index.js
@@ -1,145 +1,145 @@
-"use strict";
+"use strict"
 
 // npm
-const fast = require("fast-json-stable-stringify");
+const fast = require("fast-json-stable-stringify")
 
 // core
-const { createHash } = require("crypto");
-const assert = require("assert").strict;
+const { createHash } = require("crypto")
+const assert = require("assert").strict
 
 class DocsDb {
   constructor(data = {}) {
-    assert.equal(typeof data, "object", "first argument should be an object");
-    this._db = new Map(data.db);
-    this._history = new Map(data.history);
-    this._onlyMetas = this._onlyMetas.bind(this);
+    assert.equal(typeof data, "object", "first argument should be an object")
+    this._db = new Map(data.db)
+    this._history = new Map(data.history)
+    this._onlyMetas = this._onlyMetas.bind(this)
   }
 
   get export() {
     return {
       db: Array.from(this._db),
-      history: Array.from(this._history)
-    };
+      history: Array.from(this._history),
+    }
   }
 
   get historyCount() {
     return (
       this._history.size +
       Array.from(this._history.values()).reduce((a, b) => a + b.length, 0)
-    );
+    )
   }
 
   get docCount() {
-    return this._db.size;
+    return this._db.size
   }
 
   get docIds() {
-    return this._db.keys();
+    return this._db.keys()
   }
 
   get docMetas() {
-    return Array.from(this._db.values()).map(this._onlyMetas);
+    return Array.from(this._db.values()).map(this._onlyMetas)
   }
 
   get sizeCurrent() {
-    return JSON.stringify(Array.from(this._db)).length;
+    return JSON.stringify(Array.from(this._db)).length
   }
 
   get sizeHistory() {
-    return JSON.stringify(Array.from(this._history)).length;
+    return JSON.stringify(Array.from(this._history)).length
   }
 
   get size() {
-    return this.sizeCurrent + this.sizeHistory;
+    return this.sizeCurrent + this.sizeHistory
   }
 
   deleteDoc(_id, currentRev) {
-    return this.updateDoc({ _id, _deleted: true }, currentRev);
+    return this.updateDoc({ _id, _deleted: true }, currentRev)
   }
 
   revertDoc(_id, currentRev, to) {
-    const versions = this._history.get(_id);
-    assert(versions.length, "nothing to revert");
+    const versions = this._history.get(_id)
+    assert(versions.length, "nothing to revert")
 
-    if (to === undefined) to = versions.length - 1;
-    assert.equal(typeof to, "number", "to should be a number");
-    assert(to >= 0, "to should be a positive number");
-    return this.updateDoc(versions[Math.floor(to)], currentRev);
+    if (to === undefined) to = versions.length - 1
+    assert.equal(typeof to, "number", "to should be a number")
+    assert(to >= 0, "to should be a positive number")
+    return this.updateDoc(versions[Math.floor(to)], currentRev)
   }
 
   updateDoc(newDoc, currentRev) {
     assert.equal(
       typeof newDoc,
       "object",
-      "expecting an object as a first argument"
-    );
+      "expecting an object as a first argument",
+    )
     assert.equal(
       typeof newDoc._id,
       "string",
-      "expecting a string for _id field"
-    );
-    const oldDoc = this._db.get(newDoc._id) || {};
-    const newDocHash = this._getHash(newDoc);
-    let v;
+      "expecting a string for _id field",
+    )
+    const oldDoc = this._db.get(newDoc._id) || {}
+    const newDocHash = this._getHash(newDoc)
+    let v
     if (oldDoc._rev) {
       assert.equal(
         oldDoc._rev,
         currentRev,
-        `revs should match ${oldDoc._rev} --- ${currentRev}`
-      );
-      const [a, oldDocHash] = oldDoc._rev.split("-");
-      if (oldDocHash === newDocHash) return oldDoc;
-      v = parseInt(a, 10) + 1;
-      this._updateHistory(oldDoc);
+        `revs should match ${oldDoc._rev} --- ${currentRev}`,
+      )
+      const [a, oldDocHash] = oldDoc._rev.split("-")
+      if (oldDocHash === newDocHash) return oldDoc
+      v = parseInt(a, 10) + 1
+      this._updateHistory(oldDoc)
     } else {
-      v = 0;
-      this._history.set(newDoc._id, []);
+      v = 0
+      this._history.set(newDoc._id, [])
     }
 
-    const _updated = new Date().toISOString();
-    const _created = oldDoc._created || _updated;
-    const _rev = `${v}-${newDocHash}`;
+    const _updated = new Date().toISOString()
+    const _created = oldDoc._created || _updated
+    const _rev = `${v}-${newDocHash}`
     const ret = {
       ...newDoc,
       _rev,
       _created,
-      _updated
-    };
-    this._db.set(ret._id, ret);
-    return ret;
+      _updated,
+    }
+    this._db.set(ret._id, ret)
+    return ret
   }
 
   // private
   _onlyMetas(doc) {
-    let r;
-    const out = {};
+    let r
+    const out = {}
     for (r in doc) {
-      if (r[0] === "_") out[r] = doc[r];
+      if (r[0] === "_") out[r] = doc[r]
     }
-    return out;
+    return out
   }
 
   _dropMetas(doc) {
-    let r;
-    const out = {};
+    let r
+    const out = {}
     for (r in doc) {
-      if (r[0] !== "_") out[r] = doc[r];
+      if (r[0] !== "_") out[r] = doc[r]
     }
-    return out;
+    return out
   }
 
   _getHash(doc) {
-    const h = createHash("md5");
-    h.update(fast(this._dropMetas(doc)));
+    const h = createHash("md5")
+    h.update(fast(this._dropMetas(doc)))
     return h
       .digest("base64")
       .replace(/[-/+]/g, "")
-      .replace(/=+$/g, "");
+      .replace(/=+$/g, "")
   }
 
   _updateHistory(doc) {
-    this._history.get(doc._id).push(doc);
+    this._history.get(doc._id).push(doc)
   }
 }
 
-module.exports = DocsDb;
+module.exports = DocsDb
