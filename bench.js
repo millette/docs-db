d69f2f82009605472b69e13b67122be6fbd72d6f
Robin Millette <robin@millette.info>
2018-12-12 17:09:06 -0500

    Touchup to bench thingy.

--- bench.js
+++ bench.js
@@ -2,7 +2,7 @@
 
 // core
 const os = require("os")
-// const fs = require('fs')
+const fs = require("fs")
 
 const freeMem0 = os.freemem()
 
@@ -12,7 +12,7 @@ const chance = require("chance").Chance()
 // self
 const DocsDb = require(".")
 
-const docs = require("./out-fake.json")
+// const docs = require("./out-fake.json")
 
 const makeContent = () => {
   const n = Math.round(Math.random() * 5 + 2)
@@ -40,7 +40,8 @@ const makeDoc = () => ({
 
 const bench = (num) => {
   const now0 = Date.now()
-  const db = new DocsDb(docs)
+  const db = new DocsDb()
+  // const db = new DocsDb(docs)
   const now = Date.now()
   const freeMem1 = os.freemem()
 
@@ -63,7 +64,7 @@ const bench = (num) => {
   console.log(db.historyCount)
   console.log(freeMem0 - freeMem1)
   console.log(Math.round((freeMem1 - freeMem2) / num))
-  // fs.writeFileSync('out-fake.json', JSON.stringify(db.export))
+  fs.writeFileSync("out-fake.json", JSON.stringify(db.export))
 }
 
 bench(5000)
