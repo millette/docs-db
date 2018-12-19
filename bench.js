f8a65ad26ed3a409d3ad9ce90d848b4be5785996
Robin Millette <robin@millette.info>
2018-12-18 19:28:02 -0500

    Smaller fake data set.

--- bench.js
+++ bench.js
@@ -41,7 +41,6 @@ const makeDoc = () => ({
 const bench = (num) => {
   const now0 = Date.now()
   const db = new DocsDb()
-  // const db = new DocsDb(docs)
   const now = Date.now()
   const freeMem1 = os.freemem()
 
@@ -64,7 +63,9 @@ const bench = (num) => {
   console.log(db.historyCount)
   console.log(freeMem0 - freeMem1)
   console.log(Math.round((freeMem1 - freeMem2) / num))
-  fs.writeFileSync("out-fake.json", JSON.stringify(db.export))
+  fs.writeFileSync("out-data.json", JSON.stringify(db.export))
+  // fs.writeFileSync("out-fake.json", JSON.stringify(db.export))
+  // fs.writeFileSync("out-pretty.json", JSON.stringify(db.export, null, '  '))
 }
 
-bench(5000)
+bench(50)
